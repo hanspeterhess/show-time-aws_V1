094995c579738023fb05bd6925619633beaa3a32
Hanspeter Hess <hanspeter.hess@unibe.ch>
2025-07-04 10:08:28 +0200

    deploy  ECS cluster - adapted cluster names in autoDeploy

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,6 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as docker from "@pulumi/docker";
+import * as awsx from "@pulumi/awsx";
 
 // Config
 const config = new pulumi.Config();
@@ -30,6 +30,97 @@ const repo = new aws.ecr.Repository("show-time-backend", {
     },
 });
 
+// 1. Create a VPC (default)
+// const vpc = new awsx.ec2.Vpc("ecs-vpc", {
+//     subnets: [{ type: "public" }],
+// });
+const vpc = new awsx.ec2.Vpc("ecs-vpc", {});
+
+// 2. Create an ECS Cluster
+const cluster = new aws.ecs.Cluster("ecs-cluster", {
+    name: "show-time-cluster",
+});
+
+// 3. IAM Role for ECS task
+const taskExecRole = new aws.iam.Role("ecsTaskExecRole", {
+    assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({ Service: "ecs-tasks.amazonaws.com" }),
+});
+
+// Attach policies to the ECS task execution role
+new aws.iam.RolePolicyAttachment("ecsTaskExecPolicy", {
+    role: taskExecRole.name,
+    policyArn: aws.iam.ManagedPolicy.AmazonECSTaskExecutionRolePolicy,
+});
+
+// 4. Create a Security Group for the ECS service
+const sg = new aws.ec2.SecurityGroup("ecs-sg", {
+    vpcId: vpc.vpc.id,
+    description: "Allow HTTP",
+    ingress: [
+        {
+            protocol: "tcp",
+            fromPort: 4000,
+            toPort: 4000,
+            cidrBlocks: ["0.0.0.0/0"],
+        },
+    ],
+    egress: [
+        {
+            protocol: "-1",
+            fromPort: 0,
+            toPort: 0,
+            cidrBlocks: ["0.0.0.0/0"],
+        },
+    ],
+});
+
+// 5. Create an ECS Task Definition
+const taskDefinition = new aws.ecs.TaskDefinition("ecs-task", {
+    family: "show-time-task",
+    networkMode: "bridge",
+    cpu: "256",
+    memory: "512",
+    requiresCompatibilities: ["EC2"],
+    executionRoleArn: taskExecRole.arn,
+    containerDefinitions: pulumi.all([repo.repositoryUrl]).apply(([imageUrl]) =>
+        JSON.stringify([
+            {
+                name: "show-time-backend",
+                image: `${imageUrl}:latest`,
+                essential: true,
+                portMappings: [
+                    {
+                        containerPort: 4000,
+                        hostPort: 4000,
+                        protocol: "tcp",
+                    },
+                ],
+                environment: [
+                    { name: "AWS_REGION", value: region },
+                    { name: "TABLE_NAME", value: table.name },
+                    { name: "PORT", value: "4000" },
+                ],
+            },
+        ])
+    ),
+});
+
+// 7. ECS Service
+const service = new aws.ecs.Service("ecs-service", {
+    cluster: cluster.arn,
+    desiredCount: 1,
+    launchType: "EC2",
+    taskDefinition: taskDefinition.arn,
+    deploymentMinimumHealthyPercent: 0,
+    deploymentMaximumPercent: 100,
+    networkConfiguration: undefined, // bridge mode
+});
+
+
 export const tableName = table.name;
 export const ecrRepoUrl = repo.repositoryUrl;
 export const ecrRepoName = repo.name;
+
+export const ecsServiceName = service.name;
+export const ecsClusterName = cluster.name;
+export const ecsTaskFamily = taskDefinition.family;
